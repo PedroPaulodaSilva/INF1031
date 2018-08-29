88a6a0675da27c03a8bcd8e579bdd1c03d53b87f
Pedro Paulo da Silva <pedro.ppds@gmail.com>
2018-08-29 16:56:32 -0300

    Update adivinha.lua

--- Aula 6/adivinha.lua
+++ Aula 6/adivinha.lua
@@ -1,23 +1,24 @@
-function jogada(tentativasrestantes, numerocorreto)
+function jogada(tentativasrestantes, numerocorreto, tentou)
   if tentativasrestantes == 0 then
     print("Suas tentativas terminaram.")    
   else
     print("Forneça seu palpite: ")
     palpite = io.read("*n")
     if palpite == numerocorreto then
+      tentou = tentou + 1
       print("0")
-      print("Conseguiu em "..tentativasrestantes .."tentativas")
+      print("Conseguiu em "..tentou .." tentativas")
     elseif palpite > numerocorreto then
       print("-1")
-      jogada(tentativasrestantes-1,numerocorreto)
+      jogada(tentativasrestantes-1,numerocorreto,tentou+1)
     elseif palpite < numerocorreto then
       print("1")
-      jogada(tentativasrestantes-1,numerocorreto)
+      jogada(tentativasrestantes-1,numerocorreto,tentou+1)
     end
   end
     
 end
 math.randomseed(os.time())
 resposta = math.random(1024)
-print("a cola é",resposta)
-jogada(20, resposta)
+
+jogada(20, resposta,0)

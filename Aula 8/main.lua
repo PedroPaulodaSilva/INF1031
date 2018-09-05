a3748f5ff6c0f7ff06d0ba34334dacdb5fb94fe4
Pedro Paulo da Silva <pedro.ppds@gmail.com>
2018-09-05 16:57:41 -0300

    Update main.lua

--- Aula 8/main.lua
+++ Aula 8/main.lua
@@ -1,1 +1,32 @@
-kkk
+function love.draw()  
+  
+  local hora= os.date("*t")  
+  local w, h = love.graphics.getDimensions()
+  local espessura = 5
+  local angulo = 0
+  
+  love.graphics.translate(w/2,h/2)
+  
+  love.graphics.setBackgroundColor(0.5,0.5,0.5)  
+  love.graphics.setColor(0,0.7,0)
+  love.graphics.circle("fill", 0 ,0 , 280)
+  
+    
+  love.graphics.setColor(1,0,0)
+  love.graphics.rotate(50)
+  love.graphics.rectangle("fill", 0, -2.5, 275, espessura)
+  
+    
+  love.graphics.setColor(0,0,0)
+  love.graphics.rotate(50)
+  love.graphics.rectangle("fill", 0, -2.5, 240, espessura)
+  
+    
+  love.graphics.setColor(1,1,0)
+  love.graphics.rotate(50)
+  love.graphics.rectangle("fill", 0, -2.5, 205, espessura)
+  
+  love.graphics.setColor(0,0,0)
+  love.graphics.circle("fill", 0, 0, espessura)
+  
+end

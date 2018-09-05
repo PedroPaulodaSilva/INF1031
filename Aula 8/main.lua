eb89a47cb463e95f6431ba92f819839db660f7c0
Pedro Paulo da Silva <pedro.ppds@gmail.com>
2018-09-05 17:01:41 -0300

    Update main.lua

--- Aula 8/main.lua
+++ Aula 8/main.lua
@@ -11,20 +11,23 @@ function love.draw()
   love.graphics.setColor(0,0.7,0)
   love.graphics.circle("fill", 0 ,0 , 280)
   
-    
+  angulo = (2*math.pi/360) * hora.sec 
   love.graphics.setColor(1,0,0)
-  love.graphics.rotate(50)
+  love.graphics.rotate(angulo)
   love.graphics.rectangle("fill", 0, -2.5, 275, espessura)
+  love.graphics.rotate(-angulo)
   
-    
+  angulo = (2*math.pi/360) * hora.min  
   love.graphics.setColor(0,0,0)
-  love.graphics.rotate(50)
+  love.graphics.rotate(angulo)
   love.graphics.rectangle("fill", 0, -2.5, 240, espessura)
+  love.graphics.rotate(-angulo)
   
-    
+  angulo = (2*math.pi/60) * hora.hour   
   love.graphics.setColor(1,1,0)
-  love.graphics.rotate(50)
+  love.graphics.rotate(angulo)
   love.graphics.rectangle("fill", 0, -2.5, 205, espessura)
+  love.graphics.rotate(angulo)
   
   love.graphics.setColor(0,0,0)
   love.graphics.circle("fill", 0, 0, espessura)

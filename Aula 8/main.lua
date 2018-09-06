1b6f63b959919adbaea208d3e2f7e8093f018543
Pedro Paulo da Silva <pedro.ppds@gmail.com>
2018-09-06 17:23:07 -0300

    Update main.lua

--- Aula 8/main.lua
+++ Aula 8/main.lua
@@ -4,32 +4,41 @@ function love.draw()
   local w, h = love.graphics.getDimensions()
   local espessura = 5
   local angulo = 0
+  local st = string.format("%d", hora.day)
   
+  local fonte  =  love.graphics.newFont(32)
+  local texto  =  love.graphics.newText(fonte,st)
+  local tw, th = texto:getDimensions()
+    
   love.graphics.translate(w/2,h/2)
-  
+    
   love.graphics.setBackgroundColor(0.5,0.5,0.5)  
   love.graphics.setColor(0,0.7,0)
   love.graphics.circle("fill", 0 ,0 , 280)
   
-  angulo = (2*math.pi/360) * hora.sec 
+  angulo = (2*math.pi/60) * hora.sec - math.pi/2
   love.graphics.setColor(1,0,0)
   love.graphics.rotate(angulo)
   love.graphics.rectangle("fill", 0, -2.5, 275, espessura)
   love.graphics.rotate(-angulo)
   
-  angulo = (2*math.pi/360) * hora.min  
+  angulo = (2*math.pi/60) * hora.min - math.pi/2
   love.graphics.setColor(0,0,0)
   love.graphics.rotate(angulo)
   love.graphics.rectangle("fill", 0, -2.5, 240, espessura)
   love.graphics.rotate(-angulo)
   
-  angulo = (2*math.pi/60) * hora.hour   
+  angulo = (2*math.pi/12) * hora.hour - math.pi/2
   love.graphics.setColor(1,1,0)
   love.graphics.rotate(angulo)
-  love.graphics.rectangle("fill", 0, -2.5, 205, espessura)
-  love.graphics.rotate(angulo)
+  love.graphics.rectangle("fill", 0, -2.5, 120, espessura)
+  love.graphics.rotate(-angulo)
   
   love.graphics.setColor(0,0,0)
-  love.graphics.circle("fill", 0, 0, espessura)
+  love.graphics.circle("fill", 0, 0, espessura)  
+  
+  love.graphics.draw(texto, -tw/2 , th)
+  
+  
   
 end

7a3e1cef93df3309c8db3fd504167448ed66ffe5
Pedro Paulo da Silva <pedro.ppds@gmail.com>
2018-09-10 16:24:38 -0300

    Update numerosprimos.lua

--- Aula 9/numerosprimos.lua
+++ Aula 9/numerosprimos.lua
@@ -15,12 +15,22 @@ function imprimePrimos(x)
 end
 
 function imprimePrimeirosPrimos(n)
-  for i = 2, n do
+  local cont = 0
+  local i = 2
+  while cont < n do
     if primo(i) == true then
-      
+      print(i)
+      cont = cont + 1 
+    end
+    i = i + 1  
+  end
+end
+
 
 print("vamos imprimir todos os primos menores que X. Entre com X: ")
 x = io.read("*n")
 imprimePrimos(x)
-
+print("Vamos imprimir os n primeiros primos. Entre com N: ")
+n = io.read("*n")
+imprimePrimeirosPrimos(n)
   

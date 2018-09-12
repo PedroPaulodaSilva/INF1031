e044009e92532794c9a011a167a0ca5288ae1b78
Pedro Paulo da Silva <pedro.ppds@gmail.com>
2018-09-12 15:30:34 -0300

    Update mdc.lua

--- Aula 9/mdc.lua
+++ Aula 9/mdc.lua
@@ -1,4 +1,4 @@
-function mdc(x,y)
+function mdc1(x,y)
   r = x % y
   if r == 0 then
     return y
@@ -16,3 +16,21 @@ function mdc(x,y)
   
   return y
 end
+
+function mdc2(x,y)
+  r = x % y
+  if r == 0 then
+    return y
+  end
+  return mdc2(y,r)
+end
+
+--print(mdc1(1800,2020))
+--print(mdc2(1800,2020))
+
+print("Forneça dois números para descobrir o MDC: ")
+a, b = io.read("*n","*n")
+print("Olha que mdc iterativo bonito:")
+print(mdc1(a,b))
+print("Agora o mdc recursivo igualmente bonito:")
+print(mdc2(a,b))
